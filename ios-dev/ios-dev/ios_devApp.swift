f7c38f9da4353cd02ce04c296fab7dff392327e2
Housu Zhang <housuzhang@Housus-MacBook-Air.local>
2023-09-07 11:26:02 +0800

    feat: first commit

---
+++ ios-dev/ios-dev/ios_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_devApp.swift
+//  ios-dev
+//
+//  Created by Housu Zhang on 7/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
